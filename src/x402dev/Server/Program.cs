5ef33a24d1e55e804b37815246398884fa86b439
Michiel Post <michiel@michielpost.nl>
2026-03-11 20:21:42 +0100

    Force redeploy

--- src/x402dev/Server/Program.cs
+++ src/x402dev/Server/Program.cs
@@ -16,7 +16,7 @@ using x402dev.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. 
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
